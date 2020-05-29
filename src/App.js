1a8d4b6344987068b2267f2447d9a2d97dd39f76
Nahla Shiri <nahla.job@gmail.com>
2020-05-29 13:31:20 +0100

    correction redux checkpoint

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux'
+
 import { Provider } from 'react-redux'
 import './App.css'
 import Counter from './components/counter.js'
+import store from './store.js'
 
-
-const initialState = {
-  count: 0
- };
-
- const logger = store => next => action => {
-  console.log('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  return result
- }
- 
- const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1
-      };
-    case 'DECREMENT':
-      return {
-        count: state.count - 1
-      };
-    case 'RESET':
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
- };
-
- const store = createStore(reducer, applyMiddleware(logger));
 function App() {
 return (
 <Provider store={store}>
